023b66d15e8f0fbbd7f2e44072566d887b64e261
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2025-03-12 17:04:39 -0700

    build: include freetype as zig dependency

--- build.zig
+++ build.zig
@@ -12,11 +12,6 @@ pub fn build(b: *std.Build) void {
     const check_step = b.step("check", "");
     check_step.dependOn(&exe.step);
 
-    const freetype_build_command = b.addSystemCommand(&.{ "zig", "build" });
-    freetype_build_command.setCwd(b.path("freetype"));
-    freetype_build_command.addCheck(.{ .expect_term = .{ .Exited = 0 } });
-    exe.step.dependOn(&freetype_build_command.step);
-
     exe.linkLibC();
     exe.linkSystemLibrary("m");
 
@@ -35,9 +30,11 @@ pub fn build(b: *std.Build) void {
     });
     exe.linkLibrary(raylib.artifact("raylib"));
 
-    exe.addIncludePath(b.path("./freetype/zig-out/include"));
-    exe.addLibraryPath(b.path("./freetype/zig-out/lib/"));
-    exe.linkSystemLibrary("freetype");
+    const freetype = b.dependency("freetype", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.linkLibrary(freetype.artifact("freetype"));
 
     exe.addIncludePath(b.path("./"));
 
