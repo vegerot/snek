aa63cd8e9950fc5668dd3fef602f447fb425c60d
Max 👨🏽‍💻 Coplan <mchcopl@gmail.com>
2026-03-19 22:14:55 -0700

    perf: use 240Hz on Linux+Wayland

--- build.zig
+++ build.zig
@@ -43,6 +43,7 @@ pub fn build(b: *std.Build) void {
     const raylib = b.dependency("raylib", .{
         .target = target,
         .optimize = optimize,
+        .linux_display_backend = .Both,
     });
     exe.linkLibrary(raylib.artifact("raylib"));
 
